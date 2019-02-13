f484de340616fe5d0f58ed2f457f3d6b258c194d
U_35007842 <U_35007842@DESKTOP-AA2PECH>
2019-02-13 11:34:42 +0000

    Paso 2

---
+++ src/ETS_Grupal/rename.java
@@ -0,0 +1,11 @@
+
+package ETS_Grupal;
+public class rename {
+    int contadorpares=0;
+    for(int i=0;i<10;i++){
+    if(i%2==0){
+    contadorpares++;
+    }
+    System.out.println("hay"+contadorpares+"números pares del 0 al 10");
+    }
+}
