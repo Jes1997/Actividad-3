70dc48a85364013ef2cafd63550dc99dcf8f7b47
U_35007842 <U_35007842@172.30.33.188>
2019-02-13 11:32:38 +0000

    Versión original

---
+++ src/ETS_Grupal/ETS.java
@@ -0,0 +1,15 @@
+package ETS_Grupal;
+
+/**
+ * @author U_35007842
+ */
+public class ETS {
+     int num=0;
+    for(int i=0;i<10;i++){
+    if(i%2==0){
+        num++;
+   
+    }
+System.out.println("hay"+num+"números pares del 0 al 10");
+}
+}
